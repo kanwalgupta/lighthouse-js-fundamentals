398135ab762f90606cef51df3f9c356d2c2912c3
kanwalgupta <kanwal.gupta1@gmail.com>
2018-07-05 05:12:31 +0000

    Adds looping exercises

---
+++ ingredients.js
@@ -0,0 +1,7 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+for (var i = (ingredients.length - 1); i >=0; i--) {
+  console.log(ingredients[i]);
+}
